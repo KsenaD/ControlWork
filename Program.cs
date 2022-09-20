6a26e2dbe2e16410c440810a71bd3026aea4f47f
Ksena <Ksena461@mail.ru>
2022-09-21 01:06:11 +0300

    добавили программу решения задачи

---
+++ Program.cs
@@ -0,0 +1,14 @@
+﻿string [] Array = new string [3]{"один", "два", "202"};
+List<string> Result = new List<string>();
+for (int i = 0; i < Array.Length; i++)
+{
+    if (Array[i].Length<=3)
+    {
+        Result.Add(Array[i]);
+    }
+}
+Console.Write ($"Массив из строк, длина которых меньше или равна 3 символам: ");
+for (int i = 0; i < Result.Count; i++)
+{
+Console.Write (Result[i] + " ");
+}
